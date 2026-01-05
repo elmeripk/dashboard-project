79fd6f526dc492ff62cc693edd95ed9ebc2a5d45
Elmeri Pohjois-Koivisto <elmeripeekoo@gmail.com>
2026-01-05 04:03:12 +0200

    Update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,10 +11,15 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+      // Forward /dashboard/api/v1/* to Express
+      '/dashboard/api/v1': {
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => {
+          // Remove '/dashboard/api/v1' prefix
+          return path.replace(/^\/dashboard\/api\/v1/, '')
+        },
       },
     },
   },
